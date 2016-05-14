63bf4cc7a4ec419134f97213a2d96d54f84e0740
pitty-89 <pitty_89@mail.ru>
2016-05-14 19:02:52 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,208 @@
+<?require_once("header.php");?>
+<div class="container">
+    <div class="row">
+        <h1 class="text-center"><i class="fa fa-bell"></i> Шапка opashow</h1>
+    </div>
+    <div class="row"><hr></div>
+    <div class="row">
+        <h1 class="text-center"><i class="fa fa-bug"></i> Тело opashow</h1>
+    </div>
+    <div class="row"><hr></div>
+    <div class="row">
+        <div class="sl-container" id="slider">
+            <div class="sl-list">
+                <ul>
+                    <li class="sl-list-elem sl-active">
+                        <div class="sl-img">
+                            <img src="img/4.jpg" alt="img_2329">
+                        </div>
+                        <div class="sl-title">МЫ ПЕРЕЕХАЛИ! СТУДИЯ ЕЩЕ ЯРЧЕ И БОЛЬШЕ!</div>
+                        <div class="sl-desc">ул. СОЮЗНАЯ 4 (Автовокзал)</div>
+                        <div class="sl-line"></div>
+                        <div class="sl-href">
+                            <a href="#">Подробнее</a>
+                        </div>
+                    </li>
+                    <li class="sl-list-elem">
+                        <div class="sl-img">
+                            <img src="img/3.jpg" alt="img_2329">
+                        </div>
+                        <div class="sl-title">Программа от 2500 руб.</div>
+                        <div class="sl-desc">НОВЫЕ СКИДКИ!</div>
+                        <div class="sl-line"></div>
+                        <div class="sl-href">
+                            <a href="">Подробнее</a>
+                        </div>
+                    </li>
+                    <li class="sl-list-elem">
+                        <div class="sl-img">
+                            <img src="img/6.jpg" alt="img_2329">
+                        </div>
+                        <div class="sl-title">Шоу Трансформеров</div>
+                        <div class="sl-desc">10.000 руб! СУПЕР БОМБА!!!</div>
+                        <div class="sl-line"></div>
+                        <div class="sl-href">
+                            <a href="">Подробнее</a>
+                        </div>
+                    </li>
+                    <li class="sl-list-elem">
+                        <div class="sl-img">
+                            <img src="img/1.jpg" alt="img_2329">
+                        </div>
+                        <div class="sl-title">от 4500 руб. Удивите гостей...</div>
+                        <div class="sl-desc">ШОУ ФОКУСОВ И ИЛЛЮЗИЙ</div>
+                        <div class="sl-line"></div>
+                        <div class="sl-href">
+                            <a href="">Подробнее</a>
+                        </div>
+                    </li>
+                    <li class="sl-list-elem">
+                        <div class="sl-img">
+                            <img src="img/5.jpg" alt="img_2329">
+                        </div>
+                        <div class="sl-title">Передвижная химическая лаборатория</div>
+                        <div class="sl-desc">НАУЧНОЕ ШОУ "УНИКУМ"</div>
+                        <div class="sl-line"></div>
+                        <div class="sl-href">
+                            <a href="">Подробнее</a>
+                        </div>
+                    </li>
+                    <li class="sl-list-elem">
+                        <div class="sl-img">
+                            <img src="img/2.jpg" alt="img_2329">
+                        </div>
+                        <div class="sl-title">Побывайе внутри пузыря</div>
+                        <div class="sl-desc">ШОУ МЫЛЬНЫХ ПУЗЫРЕЙ "МОРЕ БУМ"</div>
+                        <div class="sl-line"></div>
+                        <div class="sl-href">
+                            <a href="">Подробнее</a>
+                        </div>
+                    </li>
+                </ul>
+                <div class="sl-timeline progress progress-striped active start-bar">
+                    <div id="progressbar" class="progress-bar progress-bar-success"
+                         role="progressbar"
+                         aria-valuenow="0"
+                         aria-valuemin="0"
+                         aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="sl-load"><img src="img/loading_ajax.gif" alt="loading..."></div>
+            <div class="sl-nav sl-nav-left">
+                <a href="#"><i class="fa fa-chevron-left"></i></a>
+            </div>
+            <div class="sl-nav sl-nav-right">
+                <a href="#"><i class="fa fa-chevron-right"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="row"><hr></div>
+    <div class="row">
+        <div class="item-hover">
+            <a href="#">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2 img">
+                            <img src="img/blue.png" alt="ОПА - общество профессионаьных артистов">
+                            <img class="abs" src="img/Мишень.png" alt="ОПА - общество профессионаьных артистов">
+                        </div>
+                        <div class="col-lg-10">
+                            <h3>ОПА - общество профессионаьных артистов</h3>
+                            <p>
+                                Мы работаем только с проверенными людьми, способными превратить день рождения в праздник
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="item-hover">
+            <a href="#">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2 img">
+                            <img class="abs" src="img/Сердце-3%20(2).png" alt="Клиентоориентированный подход к оганизации праздника">
+                            <img src="img/blue.png" alt="Клиентоориентированный подход к оганизации праздника">
+                        </div>
+                        <div class="col-lg-10">
+                            <h3>Клиентоориентированный подход к оганизации праздника</h3>
+                            <p>
+                                Создание торжества - индивидуальный процесс. Ваши предпочтения и пожелания формируют
+                                общую атмосферу праздника. Мы поможем выбрать "вкусную" начинку вашего праздника:
+                                световые эффекты, воздушные шары, мыльные пузыри, научное химическое
+                                шоу и многое другое! У фантазии предела нет!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="item-hover">
+            <a href="#">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2 img">
+                            <img src="img/blue.png" alt="Самые актуальные герои!">
+                            <img class="abs" src="img/Цветочек.png" alt="Самые актуальные герои!">
+                        </div>
+                        <div class="col-lg-10">
+                            <h3>Самые актуальные герои!</h3>
+                            <p>
+                                Выбор персонажа - дело не простое! Учесть все пожелания наших детей невозможно!
+                                Но пригласить их любимого героя можно всегда. Для вас мы подготовили самых актуальных
+                                и популярных киногероев и персонажей мультипликационных фильмов: пираты, феи, человек
+                                паук, миньоны, клоуны, железный человек и многие другие известные герои!
+                                Мы стремимся брать только серьёзные качественные костюмы, которые позволяют нашим
+                                артистам полностью перевоплотиться и провести волшебный праздник!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="item-hover">
+            <a href="#">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2 img">
+                            <img class="abs" src="img/Шляпа%20(2).png" alt="Демократичные цены и отзывы">
+                            <img src="img/blue.png" alt="Демократичные цены и отзывы">
+                        </div>
+                        <div class="col-lg-10">
+                            <h3>Демократичные цены и отзывы</h3>
+                            <p>
+                                У нас действую специальные акции, для постоянных клиентов предусмотрены скидки.<br/>
+                                Мы много лет работаем в сфере детских праздников и за это время мы всегда получаем
+                                исключительно положительные отзывы по своей работе.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="item-hover">
+            <a href="#">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2 img">
+                            <img class="abs" src="img/Ящик%20(2).png" alt="Только качетвенный праздник">
+                            <img src="img/blue.png" alt="Только качетвенный праздник">
+                        </div>
+                        <div class="col-lg-10">
+                            <h3>Только качетвенный праздник</h3>
+                            <p>
+                                Мы гарантируем, что мероприятие любого уровня пройдёт успешно.
+                                Потому что нас рекомендуют друзьям.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="row"><hr></div>
+    <div class="row">
+        <h1 class="text-center"><i class="fa fa-bullhorn"></i> Футер opashow</h1>
+    </div>
+</div>
+<?require_once("footer.php");?>
