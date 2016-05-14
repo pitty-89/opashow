503871bfe1d27dfd2a825654ef00a9100965a82a
pitty-89 <pitty_89@mail.ru>
2016-05-14 19:06:44 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCKTYPE html>
+<html>
+    <head>
+        <title>Сладер для сайта opashow</title>
+
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/slider.css">
+
+        <script type="text/javascript" src="jquery/jquery-2.2.3.min.js"></script>
+        <script type="text/javascript" src="jquery/bootstrap.min.js"></script>
+        <script type="text/javascript" src="jquery/imagesloaded.pkgd.min.js"></script>
+        <script type="text/javascript" src="jquery/slider.js"></script>
+    </head>
